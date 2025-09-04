97e463832ae4bbee4d81249be7fe0c77b7814791
manubolusivananda <manubolusivananda@gmail.com>
2025-09-04 11:08:54 +0530

    test.tf

---
+++ test.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+  ami                         = "ami-0b016c703b95ecbe4" # ✅ Amazon Linux 2 in us-east-1
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.multi_port.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "my-ec2"
+  }
+} 
