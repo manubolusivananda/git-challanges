5d86971e37d388b8f160d6598a4d81edef80c1e0
manubolusivananda <manubolusivananda@gmail.com>
2025-09-04 11:21:41 +0530

    gfh

--- test.tf
+++ test.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "siva" {
+resource "aws_instance" "ram" {
   ami                         = "ami-0b016c703b95ecbe4" # ✅ Amazon Linux 2 in us-east-1
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.multi_port.id]
